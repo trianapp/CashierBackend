7c175e042908ab49a0832b6ab29fb7bffe563dbe
trian damai <triannurizkillah@gmail.com>
2021-12-28 09:15:03 +0700

    feat: update docs

--- src/test/kotlin/app/trian/cashierservice/CashierServiceApplicationTests.kt
+++ src/test/kotlin/app/trian/cashierservice/CashierServiceApplicationTests.kt
@@ -1,9 +1,11 @@
 package app.trian.cashierservice
 
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@WebMvcTest
 class CashierServiceApplicationTests {
 
     @Test
